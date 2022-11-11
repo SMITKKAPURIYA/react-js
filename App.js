a0c80d630c1a2f361860cdd45ecf1074346d9ae3
Smit Kapuriya <117984241+SMITKKAPURIYA@users.noreply.github.com>
2022-11-11 03:45:12 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Welcome</code> To cosysinfotech.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.cosysinfotech.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Cosys Infotech LLP
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
